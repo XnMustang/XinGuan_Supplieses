87b26b056bfe7387592c609b3cca60b4f9d2c952
xi_nian <1617171230@qq.com>
2020-12-03 04:02:55 +0800

    测试用户分页查询-0.13

--- xinguan-base-web/src/main/java/com/forward/system/controller/UserController.java
+++ xinguan-base-web/src/main/java/com/forward/system/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.forward.system.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.forward.response.Result;
+import com.forward.system.entity.User;
+import com.forward.system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-12-03
  */
 @RestController
-@RequestMapping("/system/user")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/findUserList")
+    public Result findUserList(@RequestParam(required = true,defaultValue = "1") Integer currentPage,
+                               @RequestParam(required = true,defaultValue = "7") Integer size){
+        //对用户进行分页，注入用户
+        Page<User> page = new Page<>(currentPage,size);
+        Page<User> userPage = userService.page(page);
+        //总记录数
+        long total = userPage.getTotal();
+        //分页查询出的信息
+        List<User> records = userPage.getRecords();
+        return Result.ok().data("total",total).data("records",records);
+    }
 
 }
 
